e9d7da36aeb313c300f8439affa55024bd875e37
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-08-20 14:48:00 +0200

    Make some blockchain methods return undefined

--- packages/buidler-core/src/internal/buidler-evm/provider/node.ts
+++ packages/buidler-core/src/internal/buidler-evm/provider/node.ts
@@ -546,23 +546,17 @@ export class BuidlerNode extends EventEmitter {
   }
 
   public async getBlockByNumber(blockNumber: BN): Promise<Block | undefined> {
-    return this._blockchain
-      .getBlock(blockNumber)
-      .catch((blockNotFound) => undefined);
+    return this._blockchain.getBlock(blockNumber);
   }
 
   public async getBlockByHash(blockHash: Buffer): Promise<Block | undefined> {
-    return this._blockchain
-      .getBlock(blockHash)
-      .catch((blockNotFound) => undefined);
+    return this._blockchain.getBlock(blockHash);
   }
 
   public async getBlockByTransactionHash(
     hash: Buffer
   ): Promise<Block | undefined> {
-    return this._blockchain
-      .getBlockByTransactionHash(hash)
-      .catch((transactionNotFound) => undefined);
+    return this._blockchain.getBlockByTransactionHash(hash);
   }
 
   public async getBlockTotalDifficulty(block: Block): Promise<BN> {
@@ -589,12 +583,8 @@ export class BuidlerNode extends EventEmitter {
     return this._nextBlockTimestamp;
   }
 
-  public async getSuccessfulTransactionByHash(
-    hash: Buffer
-  ): Promise<Transaction | undefined> {
-    return this._blockchain
-      .getTransaction(hash)
-      .catch((transactionNotFound) => undefined);
+  public async getTransaction(hash: Buffer): Promise<Transaction | undefined> {
+    return this._blockchain.getTransaction(hash);
   }
 
   public async getTransactionReceipt(
@@ -819,7 +809,10 @@ export class BuidlerNode extends EventEmitter {
       i.lte(filterParams.toBlock);
       i = i.addn(1)
     ) {
-      const block = await this._blockchain.getBlock(new BN(i));
+      const block = await this.getBlockByNumber(new BN(i));
+      if (block === undefined) {
+        continue;
+      }
       const receipts = this._blockHashToTxReceipts.get(
         bufferToHex(block.hash())
       );
