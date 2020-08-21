b9c0f4706cc282e5902e2e86b6bdfa7f6c5de894
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-08-21 09:46:26 +0200

    Handle transaction receipts inside Blockchain

--- packages/buidler-core/src/internal/buidler-evm/provider/node.ts
+++ packages/buidler-core/src/internal/buidler-evm/provider/node.ts
@@ -587,13 +587,7 @@ export class BuidlerNode extends EventEmitter {
   public async getTransactionReceipt(
     hash: Buffer
   ): Promise<RpcReceiptOutput | undefined> {
-    const block = await this.getBlockByTransactionHash(hash);
-    if (block === undefined) {
-      return undefined;
-    }
-    const index = block.transactions.findIndex((tx) => tx.hash().equals(hash));
-    const receipts = this._blockHashToReceipts.get(bufferToHex(block.hash()));
-    return receipts?.[index];
+    return this._blockchain.getTransactionReceipt(hash);
   }
 
   public async getPendingTransactions(): Promise<Transaction[]> {
@@ -1096,6 +1090,7 @@ export class BuidlerNode extends EventEmitter {
     const receipts = getRpcReceipts(block, runBlockResult);
 
     const blockHash = bufferToHex(block.hash());
+    this._blockchain.addTransactionReceipts(receipts);
     this._blockHashToReceipts.set(blockHash, receipts);
 
     const td = await this.getBlockTotalDifficulty(block);
