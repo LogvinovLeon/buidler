16c276fe98e2da2720d0898d9cfc8fa87c6c4008
Daniel Izdebski <spidiritsu@gmail.com>
2020-11-09 13:26:31 +0100

    Add TxPool snapshot id to the Snapshot type

--- packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
@@ -598,12 +598,12 @@ export class HardhatNode extends EventEmitter {
       date: new Date(),
       latestBlock: await this.getLatestBlock(),
       stateRoot: await this._stateManager.getStateRoot(),
+      txPoolSnapshotId: this._txPool.snapshot(),
       blockTimeOffsetSeconds: new BN(this._blockTimeOffsetSeconds),
       nextBlockTimestamp: new BN(this._nextBlockTimestamp),
     };
 
     this._snapshots.push(snapshot);
-    this._txPool.snapshot();
     this._nextSnapshotId += 1;
 
     return id;
@@ -635,7 +635,7 @@ export class HardhatNode extends EventEmitter {
     await this._stateManager.setStateRoot(snapshot.stateRoot);
     this._blockTimeOffsetSeconds = newOffset;
     this._nextBlockTimestamp = snapshot.nextBlockTimestamp;
-    this._txPool.revert(id);
+    this._txPool.revert(snapshot.txPoolSnapshotId);
 
     // We delete this and the following snapshots, as they can only be used
     // once in Ganache
